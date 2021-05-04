fddaa6b51997fe5d2f34508f47498a4d5ba59541
Jean Carlos <57968168+caje-vi@users.noreply.github.com>
2021-05-03 21:00:56 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "../include/Pokemon.h"
+
+int main(int argc, char *argv[]){
+    int n;
+    tPokemon* P;
+
+    scanf("%d", &n);
+    //getchar();
+    P = ListaPoke(n);
+    printf("%d\n", ReturnTipo(P));
+
+return 0;
+}
